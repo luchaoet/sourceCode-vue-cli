11becfc536da2f4eb8882093863b4a4ac7d63f85
luc <550502661@qq.com>
2017-12-05 23:48:54 +0800

    path.join

---
+++ downLoadTest/a.js
@@ -0,0 +1,7 @@
+const download = require('download-git-repo')
+// download('Lucy20209060/tem', process.cwd(), { clone: true }, function (err) {
+//     if (err) console.log(err)
+//   })
+
+const home = require('user-home')
+console.log(home) 					// /Users/Administrator.PC-20161117IECM
